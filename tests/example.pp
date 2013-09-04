4ec076ca3e9dd7e9c00ee33a7e74ed9a6647a7d3
Michael Arnold <github@razorsedge.org>
2013-09-03 23:19:14 -0700

    Fixed all lint errors in tests/example.pp.

--- tests/example.pp
+++ tests/example.pp
@@ -1,25 +1,22 @@
-class bmclib::bmc_example {
+include bmclib
+bmc { 'ipmidevice':
+  ensure   => enabled,
+  provider => 'ipmitool',
+  vlanid   => '1',
+  ip       => '192.168.1.22',
+  netmask  => '255.255.255.0',
+  gateway  => '192.168.1.1',
+  snmp     => 'public',
+  force    => true,
+  require  => Class['bmclib'],
+}
 
-
-  bmc{"ipmidevice":
-        provider => "ipmitool",
-        ensure => enabled,
-        vlanid => "1",
-        ip => "192.168.1.22",
-        netmask => "255.255.255.0",
-        gateway => "192.168.1.1",
-        snmp => "public",
-        force => true,
-        require => Class["bmclib"]
-       }
-
-    bmcuser{"bmcuser"
-       provider => "freeipmi",
-       privlevel => "admin"
-       ensure => enabled,
-       username => "username",
-       userpass => "userpass",
-       force => true,
-       require => Class["bmclib"]
-    }
+bmcuser { 'bmcuser'
+  ensure    => enabled,
+  provider  => 'freeipmi',
+  privlevel => 'admin',
+  username  => 'username',
+  userpass  => 'userpass',
+  force     => true,
+  require  => Class['bmclib'],
 }
