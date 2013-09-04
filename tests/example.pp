6981e5a413c7859e4204c82bc23059fb37644808
Michael Arnold <github@razorsedge.org>
2013-09-03 23:15:26 -0700

    Moved manifests/bmc_example.pp to tests/example.pp.

--- tests/example.pp
+++ tests/example.pp
@@ -1,1 +1,25 @@
-include bmclib::bmc_example
+class bmclib::bmc_example {
+
+
+  bmc{"ipmidevice":
+        provider => "ipmitool",
+        ensure => enabled,
+        vlanid => "1",
+        ip => "192.168.1.22",
+        netmask => "255.255.255.0",
+        gateway => "192.168.1.1",
+        snmp => "public",
+        force => true,
+        require => Class["bmclib"]
+       }
+
+    bmcuser{"bmcuser"
+       provider => "freeipmi",
+       privlevel => "admin"
+       ensure => enabled,
+       username => "username",
+       userpass => "userpass",
+       force => true,
+       require => Class["bmclib"]
+    }
+}
